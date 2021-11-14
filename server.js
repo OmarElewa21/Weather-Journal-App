5642392743758da50fb1f823f980efc895a67a9c
Hashtechs <Hashtechs.stack@gmail.com>
2021-11-14 22:46:18 +0200

    New Styling Added - server is ready - app.js ready

--- server.js
+++ server.js
@@ -1,19 +1,57 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = {
+    temp: "",
+    date: "",
+    feelings: ""
+};
 
 // Require Express to run server and routes
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
 
 // Start up an instance of app
+const app = express()
 
 /* Middleware*/
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors())
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('/website'));
 
 
-// Setup Server
+/* Routing Section  */
+// Home
+app.get('/home', function(req, res) {
+    // returns projectDate object
+    res.send(projectData)
+});
+
+
+/**
+ * Update projectData object with from data
+ * 
+ * @return projectData
+ */
+app.post('/updateProjectData', function(req, res) {
+    projectData = {
+        temp: req.body.temp,
+        date: req.body.date,
+        feelings: req.body.feelings
+    };
+    console.log(projectData);
+    res.send(projectData)
+})
+
+// Run Server
+const port = 3000;
+
+app.listen(port, ()=> {
+    console.log("Started on PORT 3000");
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,106 @@
 /* Global Variables */
+const WeatherMap_ApiKey = "00341eee1f64ec68fa01de49eb239499";
+
+let date = document.getElementById("date");
+let temp = document.getElementById("temp");
+let feelings = document.getElementById("content");
+let generate = document.getElementById("generate");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+
+/*      Helper Functions      */
+
+/**
+ * Send Post Request
+ * 
+ * @param string url, object data
+ * 
+ * @return data object or error message
+ */
+async function postData (url = '', data = {}) {
+    let res = await fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
+    });
+    try {
+        let data= await res.json();
+        return data;
+    } catch (error) {
+        console.log(`error: ${error}`);
+    }
+}
+
+
+/** 
+ * get external weather data from OpenWeatherMap API
+ * 
+ * @param zip code
+ * 
+ * @return weather data object or error message
+ */
+async function getWeatherData(zip) {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${WeatherMap_ApiKey}&units=metric`;
+    try {
+        let data = await fetch(apiUrl)
+        data = await data.json();
+        return data;
+    } catch (error) {
+        console.log(`error: ${error}`);
+    }
+}
+
+
+/**
+ * Update the UI with the new weather data based on the Zip Code
+ */
+async function update_UI() {
+    let req = await fetch('http://192.168.1.1:3000/home');
+    try {
+        let projectData = await req.json();
+        date.innerHTML = `Current Date is: &nbsp&nbsp ${projectData.date}`;
+        temp.innerHTML = `Current Temperature is: &nbsp&nbsp ${projectData.temp} degree`;
+        feelings.innerHTML = `${projectData.feelings}`;
+        
+    } catch (error) {
+        console.log(`error: ${error}`);
+    }
+}
+
+
+
+/*                  Main Functions                    */
+
+/** 
+ * Update UI with Generated Weather Data
+*/
+async function generateData() {
+    let zip = document.getElementById("zip");
+    let feelings_ = document.getElementById("feelings");
+
+    try {
+        if (zip.value.length > 0) {
+        // if zip has a value
+            getWeatherData(zip.value).then(function(data = {}){
+                postData('http://192.168.1.1:3000/updateProjectData', {
+                        date: newDate,
+                        temp: data.main.temp,
+                        feelings: feelings_.value
+                    }).then(setTimeout(function(){update_UI()}, 500));
+            });
+        }
+        else {
+            console.log("Enter Zip Value: ")
+        }
+    } catch (error) {
+        console.log(`error: ${error}`)
+    }
+}
+
+
+generate.addEventListener('click', generateData);   // Generate Data when Clicked
